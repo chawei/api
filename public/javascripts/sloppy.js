b36b3eb6a05638883cb7c784f33e599b3422dea8
yuin <stopcloud@gmail.com>
2011-04-17 12:40:05 -0700

    creat css for reviews

--- public/javascripts/sloppy.js
+++ public/javascripts/sloppy.js
@@ -2,15 +2,13 @@
 for(var i=0; i<reviews.length; i++) {
   var item = $('<div class="review_item"></div>');
   var profile = $('<div class="profile"><img src="'+ reviews[i].user.profile_image_url +'"/></div>');
-  var user_name = $('<div class="user_name">'+reviews[i].user.name+'</div>');
-	var text = $('<div class="text">'+reviews[i].text+'</div>');
-	var created_at = $('<div class="created_at">'+reviews[i].created_at+'</div>');
+  var item_info = $('<div class="item_info"></div>');
+  item_info.append('<div class="user_name">'+reviews[i].user.name+'</div>');
+	item_info.append('<div class="text">'+reviews[i].text+'</div>');
 	
 	item.append(profile);
-	item.append(user_name);
-	item.append(text);
-	item.append(created_at);
-  
+	item.append(item_info);
+  item.append('<div style="clear:both;"/>');
   $('#main').append(item);
 }
 
