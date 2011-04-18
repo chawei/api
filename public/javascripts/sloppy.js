eca13f2e0316d4ed81b64a6d29cce86bd1559d11
David Chawei Hsu <chawei.hsu@gmail.com>
2011-04-18 01:50:48 -0700

    improve sloppy interaction

--- public/javascripts/sloppy.js
+++ public/javascripts/sloppy.js
@@ -1,16 +1,31 @@
-
-for(var i=0; i<reviews.length; i++) {
-  var item = $('<div class="review_item"></div>');
-  var profile = $('<div class="profile"><img src="'+ reviews[i].user.profile_image_url +'"/></div>');
-  var item_info = $('<div class="item_info"></div>');
-  item_info.append('<div class="user_name">'+reviews[i].user.name+'</div>');
-	item_info.append('<div class="text">'+reviews[i].text+'</div>');
+$(document).ready(function() {
+  var displaySection = Math.random() > 0.5 ? '#main' : '#articles';
+  $(displaySection).fadeIn();
+  
+  var len = reviews.length;
+  for(var i=0; i<len; i++) {
+    var item = $('<div class="review_item"></div>');
+    var profile = $('<div class="profile"><img src="'+ reviews[i].user.profile_image_url +'"/></div>');
+    var item_info = $('<div class="item_info"></div>');
+    item_info.append('<div class="user_name">'+reviews[i].user.name+'</div>');
+  	item_info.append('<div class="text">'+reviews[i].text+'</div>');
 	
-	item.append(profile);
-	item.append(item_info);
-  item.append('<div style="clear:both;"/>');
-  $('#main').append(item);
-}
+  	item.append(profile);
+  	item.append(item_info);
+    item.append('<div style="clear:both;"/>');
+    $('#main').append(item);
+  }
+  
+  setInterval(function(){
+    $('.review_item:first').slideUp(function(){
+      var review = $(this);
+      review.clone().show().appendTo('#main');
+      //$('#main').append(review.html());
+      review.remove();
+    });
+  }, 2000);
+  
+})
 
 
 	
